6b39ddc31eb64d54ae3d341415a8b6bc57f68999
Martin Tran <martin.hua.tran@gmail.com>
2019-06-09 22:13:58 +0000

    updated with current Artix setup

--- init.el
+++ init.el
@@ -1,28 +1,41 @@
 (require 'package)
+
+;; Custom settings
+(show-paren-mode 1)
+(column-number-mode)
+(global-set-key "\M-g" `goto-line)
+
+;; Custom aliases
+(defalias 'rb 'revert-buffer)
+(defalias 'cr 'comment-region)
+(defalias 'ucr 'uncomment-region)
+(defalias 'irr 'indent-rigidly-right)
+(defalias 'irl 'indent-rigidly-left)
+
+;; Sane window resizing keybinds
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-view-program-list (quote (("epdfview" ((output-pdf "epdfview %s.pdf")) nil))))
- '(TeX-view-program-selection
-   (quote
-    (((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "epdfview")
-     (output-html "xdg-open"))))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
+     ("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(package-selected-packages
    (quote
-    (latex-math-preview auctex nhexl-mode markdown-mode haskell-mode))))
-(show-paren-mode 1)
-(global-set-key "\M-g" `goto-line)
+    (go-eldoc go-mode xresources-theme auctex markdown-mode haskell-mode))))
+
 (package-initialize)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
